d3293fc3df595eeb9803659cea4593d5e846d091
Humberto Bravo <humbandroid@gmail.com>
2023-04-05 15:05:33 -0300

    Revert "v0.2.0 - Preparando terreno"
    This reverts commit d8e0c58bc748129ab281622fd1cee07a174bd933.

---
+++ .config/configurations.py
@@ -0,0 +1,3 @@
+import os
+print("Devcontainer Configurations v1");
+os.system("sh presets.sh");
---
+++ .config/presets.sh
@@ -0,0 +1,4 @@
+echo "Configurate the Presets of Workspace v1"
+
+echo "Tunnel SSH"
+exec "cp .ssh/ ~/"
