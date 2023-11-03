5e4e38d2b2000f43b62f283d5b20b6859cc0ff09
Humba01 <humbandroid@gmail.com>
2023-11-03 16:32:10 -0300

    Arrumando bin/README.md

--- bin/wertsfy.cpp
+++ bin/wertsfy.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Os benefícios da reestruturação
+  // O código mais organizado e com programação orientada a objetos
+  // O controle melhor dos tipos primitivos
+  // A ausência do Not-A-Number do Javascript
+  // A criação de uma arquitetura de motor nova
+  // Interpretação de múltiplas linguagens de programação
+  // Eficiência operacional do sistema
+  // Integração com o Front-End
+  // Uso do Makefile por padrão de build
+  // Integração com pré-processadores de código com Lex e Yacc
+  // Criação de uma Interface de Programação de Aplicativos melhor
+  // Criação de uma Interface de Linha de Comando melhor e integrada
+  // Reutilização de bibliotecas antigas de outros lugares e adaptação
+  // Reutilização de interfaces legado anteriores
+  // Escalabilidade e Flexibilidade de novos módulos
+  // Uso de recursos da linguagem C e C++
+  // Transcompilação entre as linguagens de programação
+
+namespace Arithmetic { };
+namespace Conversor { };
+namespace LogicConstructor { };
+namespace Formatter { };
+namespace UserInterface { };
+namespace IOManipulations { };
+namespace ApplicationProgramInterface { };
+namespace CommandLineInterface { };
+namespace ExternalLibraries { };
+namespace Wertsfy { };
+
 int main(void) 
 {
   return 0;
