cedc4fba77a25c68014f3ee7297b923eb48be27b
Humba01 <humbandroid@gmail.com>
2023-11-09 01:46:25 -0300

    Construindo nova arquitetura - XII

---
+++ #integration/cloudmdls/README.md
@@ -0,0 +1,12 @@
+# CloudMlds
+
+## SAMPLE CONFIGURATIONS
+
+1. https://www.openstack.org/software/sample-configs/#compute-starter-kit
+2. https://www.openstack.org/software/sample-configs/#container-optimized
+3. https://www.openstack.org/software/sample-configs/#web-applications
+4. https://www.openstack.org/software/sample-configs/#big-data
+
+## COMPONENTS
+
+https://www.openstack.org/software/project-navigator/openstack-components
--- bin/wcorebase.hpp
+++ bin/wcorebase.hpp
@@ -4,31 +4,7 @@
 #include <cstdio>
 #include <cstdlib>
 
-// Estrutura de Registro
-typedef struct RegistroComponente 
-{ 
-  char wpropriedade='W';
-  char wfuncid_categoria;
-  char* wfuncid_categoria_nickid;
-  char wfuncid_tipo;
-  char wfuncid_centena;
-  char wfuncid_dezena;
-  char wfuncid_unidade;
-  char wfuncversaoid='V';
-  short int wfuncversaoid_lm_major;
-  short int wfuncversaoid_lm_minor;
-  short int wfuncversaoid_lm_patch;
-  struct RegistroComponente *wfuncreg_prox;
-  struct RegistroComponente *wfuncreg_ant;
-} registro_componente;
-
-typedef struct ListaComponentes 
-{ 
-  struct RegistroComponente *wfuncreg_inicio;
-  struct RegistroComponente *wfuncreg_fim;
-  int wfuncreg_tamanho;
-} lista_componentes;
-
-void realiza_registro() { }
+typedef struct Registro { };
+typedef struct ListaRegistros { };
 
 #endif // _WCOREBASE_HPP_
